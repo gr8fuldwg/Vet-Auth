c9508325596c39b7218dec8f3686fcdada13bbb3
gr8fuldwg <gr8fuldwg@gmail.com>
2018-12-08 13:27:29 -0500

    added dependencies to package.json. Installed body-parser, compression, helmet, logger and morgan

--- server/index.js
+++ server/index.js
@@ -3,6 +3,22 @@ const express = require('express');
 
 const app = express();
 
+//middleware: import
+const bodyParser = require('body-parser');
+const helmet = require('helmet');
+const logger = require('morgan');
+const compression = require('compression');
+
+
+
+//middleware: use
+server.use(bodyParser.json());
+server.use(bodyParser.urlencoded({ extended: true }));
+server.use(logger('dev'));
+server.use(compression());
+server.use(helmet());
+const staticPath = path.resolve(__dirname, '../client/build');
+server.use(express.static(staticPath));
 
 app.post('/api/users/signup', (req, res) => {
     res.json({
